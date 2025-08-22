5a97d91cf18b7f33a9e8ed189e90b0cc42db7e7a
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-22 02:32:40 +0100

    Update main.js
    Updated https://www.toinvoice.org/ from  https://simply-invoice-webserver.vercel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app'; 
+    iframe.src = 'https://www.toinvoice.org'; 
     document.body.appendChild(iframe);
