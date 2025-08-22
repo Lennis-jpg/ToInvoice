ca598ba0727a30007db5b909744ef9d783d4c46a
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-22 18:59:22 +0100

    Update login.js
    Deleted the sub Login at the end of https://www.toinvoice.org/

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://www.toinvoice.org/Login'; 
+    iframe.src = 'https://www.toinvoice.org'; 
     document.body.appendChild(iframe);
