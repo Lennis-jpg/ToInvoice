6f778863cfdde83daf6b25d21c24bb5ef579a72e
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-22 02:35:41 +0100

    Update login.js
    Updated Login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app/Login.html'; 
+    iframe.src = 'https://www.toinvoice.org/Login.html'; 
     document.body.appendChild(iframe);
