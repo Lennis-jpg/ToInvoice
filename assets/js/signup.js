f38a632042e4a5cc0ca44c9b4d515430da77898d
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-22 02:36:43 +0100

    Update signup.js
    Updated Signup page for www.toinvoice.org domain

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app/Signup.html'; 
+    iframe.src = 'https://www.toinvoice.org/Signup.html'; 
     document.body.appendChild(iframe);
